662317f12dc3f261245e7ae83f1625ba65d2bdfb
Marshall Brown <marshallendel@gmail.com>
2022-04-09 14:32:26 -0400

    add DOM variables to manipulate

---
+++ nyt.js
@@ -0,0 +1,17 @@
+const baseURL = 'https//api.nytimes.com/svc/search/v2/articlesearch.json'
+const key = 'HL6oQxAxBRBXeQAghrI6DYLmS8HA7VkU'
+let url;
+
+//?Search Form
+const searchTerm = document.querySelector('.search')
+const startDate = document.querySelector('.start-date')
+const endDate = document.querySelector('.end-date')
+const searchForm = document.querySelector('.submit')
+
+//?Results Navigation
+const nextBtn = document.querySelector('.next')
+const previousBtn = document.querySelector('.prev')
+const nav = document.querySelector('nav')
+
+//?Results Section
+const section = document.querySelector('section')
